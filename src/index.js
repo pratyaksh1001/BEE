2ec20730c7fbd858ddd0013fa53787b1cf0f9a43
pratyaksh <pratyakshkarmahe@gmail.com.com>
2025-07-26 12:28:05 +0530

    26-7-25

---
+++ src/index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var mongo = require("mongod");
+const port = 3000;
+const uri =
+    "mongodb+srv://pratyakshkarmahe:<db_password>@cluster0.wl92lfz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+var mongodb = new mongo(uri);
+// mongodb connection
+function run() {
+    try {
+        mongodb.connect();
+        console.log("success");
+    } catch {
+        console.log("error");
+    }
+}
+run();
+var app = express();
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(
+        "/media/pratyaksh/New Volume/github/web/public/views/home.html",
+    );
+});
+app.listen(port);
